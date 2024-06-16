35e2098b90e34500a31c6ae2fd75f89015b86ada
Nikola Zarubac <nikola.zarubac@spothopperapp.com>
2024-06-16 17:29:41 +0200

    speed insights

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,17 @@
 import { ReactNode } from "react";
 import SideNav from "../ui/dashboard/sidenav";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
-export default function DashboardLayout({children}: {children: ReactNode}) {
+export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+        {children}
+        <SpeedInsights />
+      </div>
     </div>
   )
 }
